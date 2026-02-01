dc3f6eadf8a180f77f44f43446c2409ce5f5fefc
loerting <dup2_x2@protonmail.com>
2026-02-01 13:58:24 +0100

    remove haply stuff

--- main.cpp
+++ main.cpp
@@ -99,7 +99,7 @@ ImU32 GetColor(MaterialType type) {
     return IM_COL32(0, 0, 0, 0); // Transparent
 }
 
-int __main() {
+int main() {
     // Setup Window
     if (!glfwInit()) return 1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
